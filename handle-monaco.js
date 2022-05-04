1d87045a0fdf259e3e44787b24aca875066189dd
Pro-ktmr <Pro.ktmr@gmail.com>
2022-05-04 11:23:47 +0900

    Format files

---
+++ handle-monaco.js
@@ -0,0 +1,67 @@
+var editor, session, inputEditor, inputSession, outputEditor, outputSession;
+
+window.addEventListener('load', (event) => {
+  require.config({
+    paths: {
+      'vs': './monaco-editor/min/vs'
+    },
+    'vs/nls': {
+      availableLanguages: { '*': 'ja' }
+    }
+  });
+
+  source_text = 'a = int(input())\nb = int(input())\nprint(a + b)\n';
+  input_text = '7\n5\n';
+  output_text = '';
+  if (localStorage.getItem('source_text') != null) {
+    source_text = decodeURIComponent(localStorage.getItem('source_text'));
+  }
+  if (localStorage.getItem('input_text') != null) {
+    input_text = decodeURIComponent(localStorage.getItem('input_text'));
+  }
+  if (localStorage.getItem('output_text') != null) {
+    output_text = decodeURIComponent(localStorage.getItem('output_text'));
+  }
+
+  require(['vs/editor/editor.main'], function () {
+    session = monaco.editor.createModel(source_text, 'python');
+    editor = monaco.editor.create(document.getElementById('editor'), {
+      minimap: { enabled: false },
+      automaticLayout: true,
+      renderControlCharacters: true,
+      fontSize: 20,
+      wordWrap: true,
+      renderIndentGuides: true,
+    });
+    editor.setModel(session);
+    session.onDidChangeContent((event) => {
+      localStorage.setItem('source_text', encodeURIComponent(editor.getValue()));
+    });
+
+    inputSession = monaco.editor.createModel(input_text, 'text');
+    inputEditor = monaco.editor.create(document.getElementById('input'), {
+      minimap: { enabled: false },
+      automaticLayout: true,
+      renderControlCharacters: true,
+      fontSize: 20,
+      wordWrap: true,
+    });
+    inputEditor.setModel(inputSession);
+    inputSession.onDidChangeContent((event) => {
+      localStorage.setItem('input_text', encodeURIComponent(inputEditor.getValue()));
+    });
+
+    outputSession = monaco.editor.createModel(output_text, 'text');
+    outputEditor = monaco.editor.create(document.getElementById('output'), {
+      minimap: { enabled: false },
+      automaticLayout: true,
+      renderControlCharacters: true,
+      fontSize: 20,
+      wordWrap: true,
+    });
+    outputEditor.setModel(outputSession);
+    outputSession.onDidChangeContent((event) => {
+      localStorage.setItem('output_text', encodeURIComponent(outputEditor.getValue()));
+    });
+  });
+});
