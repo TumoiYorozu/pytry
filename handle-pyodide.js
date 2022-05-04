a60dff4adb5bdc9c78588e440b520cf61c8b48d9
Pro-ktmr <Pro.ktmr@gmail.com>
2022-05-04 17:05:37 +0900

    Use Web Worker

--- handle-pyodide.js
+++ handle-pyodide.js
@@ -1,81 +1,33 @@
-async function load() {
-  let pyodide = await loadPyodide({
-    indexURL: location.href + '/pyodide',
-  });
-  await pyodide.loadPackage('numpy');
-  await pyodide.runPython(`
-import io, sys, traceback
+const worker = new Worker('worker-pyodide.js');
 
-def __run(code, input):
-    my_in = io.StringIO(input)
-    my_out = io.StringIO()
-    oldin = sys.stdin
-    oldout = sys.stdout
-    olderr = sys.stderr
-    sys.stdin = my_in
-    sys.stdout = sys.stderr = my_out
-    try:
-        exec(code, {})
-    except:
-        traceback.print_exc()
-    sys.stdin = oldin
-    sys.stdout = oldout
-    sys.stderr = olderr
-    return my_out.getvalue()`);
-  return pyodide;
+function run() {
+  outputEditor.setValue("実行中...");
+  worker.postMessage([editor.getValue(), inputEditor.getValue()]);
 }
-let pyodideReadyPromise = load();
 
-async function run() {
-  let pyodide = await pyodideReadyPromise;
-  try {
-    pyodide.globals.set("__code_to_run", editor.getValue());
-    pyodide.globals.set("__input_to_read", inputEditor.getValue().replaceAll(/\r/g, ""));
-    let output = await pyodide.runPython('__run(__code_to_run, __input_to_read)');
-    output = output.replaceAll(/(.*)File "<exec>"(.*)\n/g, "");
-    output = output.replaceAll(/(.*)File "<string>", line (\d*)(.*)\n/g, '$1File "Main.py", line $2$3\n');
-    let formatedOutput = formatOutput(output);
-    outputEditor.setValue(formatedOutput);
-
-    let err = [...formatedOutput.matchAll(/プログラムの (\d*) 行目/g)];
-    if (err != null && err.length != 0) {
-      let l = Number(err[err.length - 1][1]);
-      editor.markers = [{
-        startLineNumber: l,
-        startColumn: 1,
-        endLineNumber: l,
-        endColumn: 1000,
-        message: formatedOutput,
-        severity: monaco.MarkerSeverity.Error,
-      }];
-      monaco.editor.setModelMarkers(editor.getModel(), 'message', editor.markers);
-      outputEditor.setValue(
-        formatedOutput
-        + "\n=== エラー原文 ===\n"
-        + output
-        + "==================\n");
-    }
-    else {
-      editor.markers = [];
-      monaco.editor.setModelMarkers(editor.getModel(), 'message', editor.markers);
-    }
-  } catch (err) {
-    console.log(err);
-    outputEditor.setValue("PyTry 内部でエラーが発生しました");
+worker.addEventListener('message', (msg) => {
+  let formatedOutput = msg.data;
+  outputEditor.setValue(formatedOutput);
+  let err = [...formatedOutput.matchAll(/プログラムの (\d*) 行目/g)];
+  if (err != null && err.length != 0) {
+    let l = Number(err[err.length - 1][1]);
+    editor.markers = [{
+      startLineNumber: l,
+      startColumn: 1,
+      endLineNumber: l,
+      endColumn: 1000,
+      message: formatedOutput,
+      severity: monaco.MarkerSeverity.Error,
+    }];
+    monaco.editor.setModelMarkers(editor.getModel(), 'message', editor.markers);
+    outputEditor.setValue(
+      formatedOutput
+      + "\n=== エラー原文 ===\n"
+      + output
+      + "==================\n");
   }
-}
-
-function formatOutput(output) {
-  output = output.replaceAll('Traceback (most recent call last):\n', 'エラー発生:\n');
-  output = output.replaceAll(/(.*)File "Main.py", line (\d*)(.*)\n/g, '$1プログラムの $2 行目\n');
-  output = output.replaceAll('ZeroDivisionError: division by zero', 'ZeroDivisionError: ゼロで割ろうとしました\n');
-  output = output.replaceAll(/NameError: name '(.*)' is not defined/g, "NameError: 「$1」が見つかりません\n");
-  output = output.replaceAll('SyntaxError: invalid syntax', 'SyntaxError: 文法が間違っています\n');
-  outputEditor.setValue(output);
-  output = output.replaceAll('IndentationError: unindent does not match any outer indentation level', 'IndentationError: インデントが揃っていません\n');
-  output = output.replaceAll(/ValueError: invalid literal for int\(\) with base 10: '(.*)'/g, "ValueError: 「$1」を整数に変換できません (入力欄が正しくないことがあります)\n");
-  output = output.replaceAll('IndexError: list index out of range', 'IndexError: リストのサイズ以上の添え字の要素にアクセスしようとしました\n');
-  output = output.replaceAll(/KeyError: '(.*)'/g, 'KeyError: キー「$1」は存在しません\n');
-  output = output.replaceAll('EOFError: EOF when reading a line', 'ファイルの末尾に到達しました (入力欄が正しくないことがあります)\n');
-  return output;
-}
+  else {
+    editor.markers = [];
+    monaco.editor.setModelMarkers(editor.getModel(), 'message', editor.markers);
+  }
+});
---
+++ worker-pyodide.js
@@ -0,0 +1,63 @@
+importScripts('./pyodide/pyodide.js');
+
+async function load() {
+  let pyodide = await loadPyodide({
+    indexURL: location.href.slice(0, location.href.length - '/worker-pyodide.js'.length) + '/pyodide',
+  });
+  await pyodide.loadPackage('numpy');
+  await pyodide.runPython(`
+import io, sys, traceback
+
+def __run(code, input):
+    my_in = io.StringIO(input)
+    my_out = io.StringIO()
+    oldin = sys.stdin
+    oldout = sys.stdout
+    olderr = sys.stderr
+    sys.stdin = my_in
+    sys.stdout = sys.stderr = my_out
+    try:
+        exec(code, {})
+    except:
+        traceback.print_exc()
+    sys.stdin = oldin
+    sys.stdout = oldout
+    sys.stderr = olderr
+    return my_out.getvalue()`);
+  return pyodide;
+}
+let pyodideReadyPromise = load();
+
+async function run(source, input) {
+  let pyodide = await pyodideReadyPromise;
+  try {
+    pyodide.globals.set('__code_to_run', source);
+    pyodide.globals.set('__input_to_read', input.replaceAll(/\r/g, ""));
+    let output = await pyodide.runPython('__run(__code_to_run, __input_to_read)');
+    output = output.replaceAll(/(.*)File "<exec>"(.*)\n/g, "");
+    output = output.replaceAll(/(.*)File "<string>", line (\d*)(.*)\n/g, '$1File "Main.py", line $2$3\n');
+    let formatedOutput = formatOutput(output);
+    self.postMessage(formatedOutput);
+  } catch (err) {
+    console.log(err);
+    self.postMessage('PyTry 内部でエラーが発生しました');
+  }
+}
+
+function formatOutput(output) {
+  output = output.replaceAll('Traceback (most recent call last):\n', 'エラー発生:\n');
+  output = output.replaceAll(/(.*)File "Main.py", line (\d*)(.*)\n/g, '$1プログラムの $2 行目\n');
+  output = output.replaceAll('ZeroDivisionError: division by zero', 'ZeroDivisionError: ゼロで割ろうとしました\n');
+  output = output.replaceAll(/NameError: name '(.*)' is not defined/g, "NameError: 「$1」が見つかりません\n");
+  output = output.replaceAll('SyntaxError: invalid syntax', 'SyntaxError: 文法が間違っています\n');
+  output = output.replaceAll('IndentationError: unindent does not match any outer indentation level', 'IndentationError: インデントが揃っていません\n');
+  output = output.replaceAll(/ValueError: invalid literal for int\(\) with base 10: '(.*)'/g, "ValueError: 「$1」を整数に変換できません (入力欄が正しくないことがあります)\n");
+  output = output.replaceAll('IndexError: list index out of range', 'IndexError: リストのサイズ以上の添え字の要素にアクセスしようとしました\n');
+  output = output.replaceAll(/KeyError: '(.*)'/g, 'KeyError: キー「$1」は存在しません\n');
+  output = output.replaceAll('EOFError: EOF when reading a line', 'EOFError: ファイルの末尾に到達しました (入力欄が正しくないことがあります)\n');
+  return output;
+}
+
+self.addEventListener('message', (msg) => {
+  run(msg.data[0], msg.data[1]);
+});
