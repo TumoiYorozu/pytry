e5d60f5a0d2506f95613f389fdc02422d493f935
Pro-ktmr <Pro.ktmr@gmail.com>
2022-05-11 18:41:17 +0900

    Add error translation

--- worker-pyodide.js
+++ worker-pyodide.js
@@ -52,7 +52,8 @@ function formatOutput(output) {
   output = output.replaceAll(/NameError: name '(.*)' is not defined/g, "NameError: 「$1」が見つかりません\n");
   output = output.replaceAll('SyntaxError: invalid syntax', 'SyntaxError: 文法が間違っています\n');
   output = output.replaceAll('IndentationError: unindent does not match any outer indentation level', 'IndentationError: インデントが揃っていません\n');
-  output = output.replaceAll(/ValueError: invalid literal for int\(\) with base 10: '(.*)'/g, "ValueError: 「$1」を整数に変換できません (入力欄が正しくないことがあります)\n");
+  output = output.replaceAll(/ValueError: invalid literal for int\(\) with base 10: '(.*)'/g, "ValueError: 「$1」を整数に変換できません (入力の受け取り方や入力欄が正しくないことがあります)\n");
+  output = output.replaceAll(/ValueError: not enough values to unpack \(expected (.*), got (.*)\)/g, "ValueError: $2 個しかないデータを $1 個に分けようとしました (入力の受け取り方や入力欄が正しくないことがあります)\n");
   output = output.replaceAll('IndexError: list index out of range', 'IndexError: リストのサイズ以上の添え字の要素にアクセスしようとしました\n');
   output = output.replaceAll(/KeyError: '(.*)'/g, 'KeyError: キー「$1」は存在しません\n');
   output = output.replaceAll('EOFError: EOF when reading a line', 'EOFError: ファイルの末尾に到達しました (入力欄が正しくないことがあります)\n');
