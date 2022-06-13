809f6cb35f5c7731c6dc4a9d492b90441100a433
Pro_ktmr <60728034+Pro-ktmr@users.noreply.github.com>
2022-06-13 22:50:57 +0900

    Add Error Translation

--- worker-pyodide.js
+++ worker-pyodide.js
@@ -51,6 +51,7 @@ function formatOutput(output) {
   output = output.replaceAll('ZeroDivisionError: division by zero', 'ZeroDivisionError: ゼロで割ろうとしました\n');
   output = output.replaceAll(/NameError: name '(.*)' is not defined/g, "NameError: 「$1」が見つかりません\n");
   output = output.replaceAll('SyntaxError: invalid syntax', 'SyntaxError: 文法が間違っています\n');
+  output = output.replaceAll('IndentationError: expected an indented block', 'IndentationError: インデントを忘れています\n');
   output = output.replaceAll('IndentationError: unindent does not match any outer indentation level', 'IndentationError: インデントが揃っていません\n');
   output = output.replaceAll(/ValueError: invalid literal for int\(\) with base 10: '(.*)'/g, "ValueError: 「$1」を整数に変換できません (入力の受け取り方や入力欄が正しくないことがあります)\n");
   output = output.replaceAll(/ValueError: not enough values to unpack \(expected (.*), got (.*)\)/g, "ValueError: $2 個しかないデータを $1 個に分けようとしました (入力の受け取り方や入力欄が正しくないことがあります)\n");
